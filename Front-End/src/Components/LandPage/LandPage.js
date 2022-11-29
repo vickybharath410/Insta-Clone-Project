9d2bf78a13e9be5ff78304c8b85cd8d513449b33
vickybharath410 <vickybharath410@gmail.com>
2022-11-30 00:57:36 +0530

    changes done

--- Front-End/src/Components/LandPage/LandPage.js
+++ Front-End/src/Components/LandPage/LandPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../styles/landing.css"
 
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
  
 export default function LandingPage() {
@@ -10,7 +10,7 @@ export default function LandingPage() {
 
   return <div className='landing'>
     <div className='image-container'>
-      <img src='https://i.pinimg.com/564x/34/d6/26/34d626a4c5931da4f30077fc9f05e41e.jpg' alt='Insta Image'/>
+      <img src='https://i.pinimg.com/564x/34/d6/26/34d626a4c5931da4f30077fc9f05e41e.jpg' alt='Instaimage'/>
       
     </div>
    <button className='btn' onClick={()=>navigate('postview')}>Enter</button>
